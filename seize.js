1a91c06e8b777ae1699cb2e87a019ebf645d4e72
Idealsite <idealsite@idealsite.it>
2010-08-11 14:47:33 +0200

    Added some cool hover effects... LOL?

--- seize.js
+++ seize.js
@@ -1,52 +1,60 @@
-$(document).ready(function(){
-  var number = randomXToY(5, 5);
-  var startX = Math.floor(window.innerWidth/2) - 10;
-  var startY = Math.floor(window.innerHeight/2) - 10;
-  for (var i=1; i<=number;i++){
-    var elem = $('<div></div>'),
-    size = randomXToY(30, 100),
-    color = randomColor(),
-    finalX = randomXToY(50, (window.innerWidth - 150)),
-    finalY = randomXToY(50, (window.innerHeight - 150));
-    elem.addClass('seize')
-    .css({
+
+$('document').ready(function(){
+	var number = randomXToY(10, 20),
+	startX = Math.floor(window.innerWidth/2) - 5,
+	startY = Math.floor(window.innerHeight/2) - 5;
+	for (var i=0;i<number;i++){
+		$('#seize').append('<div class="seize" />')
+	};
+	$('.seize').each(function(i){
+		var size = randomXToY(30, 70),
+		color = randomColor(),
+		finalX = randomXToY(50, (window.innerWidth - 150)),
+		finalY = randomXToY(50, (window.innerHeight - 150)),
+		speed = Math.round(Math.sqrt(Math.pow((finalX-startX),2)+Math.pow((finalY-startY),2)))*1.5,
+		newPos = (90-size)/2-1.5;
+		$(this).css({
 			'position': 'absolute',
+			'overflow': 'hidden',
       'left' : startX,
       'top' : startY,
-      'width' : 20,
-      'height' : 20,
-      'background-color' : 'rgba(' + color + ', 0.4)',
-      'border' : '1px solid rgba(' + color + ', 0.5)',
-      '-moz-border-radius' : 100,
-      '-webkit-border-radius' : 100
-    }).animate({
-      'left' : finalX,
-      'top' : finalY,
-      'width' : size,
-      'height' : size
-      },
-      800
-    ).mouseover(function(){
+      'width' : 10,
+      'height' : 10,
+      'background-color' : 'rgba(' + color + ', 0.5)',
+      'border' : '1px solid rgba(' + color + ', 0.8)',
+      '-moz-border-radius' : 48,
+      '-webkit-border-radius' : 48
+		}).delay(400).animate({
+			'left' : finalX,
+			'top' : finalY,
+			'width' : size,
+			'height' : size
+			},speed
+		).mouseover(function(){
 			$(this).animate({
-				'border-width': '4px'
-			});
+				'width': 90,
+				'height': 90,
+				'border-width': 4,
+				'left': '-=' + newPos,
+				'top': '-=' + newPos
+				},200
+			);
+		}).mouseout(function(){
+			$(this).animate({
+				'width': size,
+				'height': size,
+				'border-width': 1,
+				'left': '+=' + newPos,
+				'top': '+=' + newPos
+				},200
+			);
 		});
-    elem.appendTo('#seize');
-  };
+	});
 });
 
 function randomXToY(minVal,maxVal){
   var randVal = Math.round(Math.random()*(maxVal-minVal)) + minVal;
   return randVal;
-};
-
-function randomEvenXToY(minVal,maxVal){
-  var randVal = Math.round(Math.random()*(maxVal-minVal)) + minVal;
-  if ((randVal % 2) == 0){ return randVal }
-  else{
-    if (randVal == maxVal){ return (--randVal) }
-    else{ return (++randVal) }
-  }
 };
 
 function randomColor(){
